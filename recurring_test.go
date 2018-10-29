3a585ffce3b0cbb86c7c7c36c80f6b03de016cb7
Ilia Choly <ilia.choly@gmail.com>
2018-10-29 18:16:03 -0400

    add more day tests

--- recurring_test.go
+++ recurring_test.go
@@ -19,10 +19,16 @@ func TestIncludes(t *testing.T) {
 		nomatch []string
 	}{
 		{
-			name:    "day",
+			name:    "Day",
 			expr:    Day(1),
 			match:   []string{"2012/01/01", "2012/12/01", "2014/02/01"},
 			nomatch: []string{"2012/01/02", "2016/07/20", "2018/08/08"},
+		},
+		{
+			name:    "Negative Day",
+			expr:    Day(-1),
+			match:   []string{"2018/10/31", "2018/11/30", "2018/02/28"},
+			nomatch: []string{"2018/10/30", "2018/11/28", "2018/02/13"},
 		},
 	}
 
